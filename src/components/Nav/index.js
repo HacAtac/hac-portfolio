7ce33a0d153a976cf5d51e73ce0c99ad8c367450
HacAtac <jhack00@icloud.com>
2021-12-16 06:42:28 -0600

    effect hook for conditional title

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Nav(props) {
-  const { currentPage, setCurrentPage } = props; //if categories is not passed in, set it to an empty array
-  //so that if categories is not passed in, it will not throw an error
-  //if categories is passed in, set it to the categories prop which is named categories in the Nav component
-  //then when the Nav component is rendered, it will pass in the categories array.
+  const { currentPage, setCurrentPage } = props;
+
+  useEffect(() => {
+    //What we've done here is used the useEffect hook to set the currentPage to the value of the currentPage prop
+    //Complicated? It is! Here's the explanation: https://reactjs.org/docs/hooks-effect.html
+    //The useEffect hook is used to run a piece of code based on a specific condition.
+    //In this case, we're using it to set the currentPage to the value of the currentPage prop
+    //When the currentPage prop changes, the useEffect hook will run and the currentPage will be set to the value of the currentPage prop
+    //This is a great way to keep track of the current page in the app
+    // setCurrentPage(props.currentPage); // setCurrentPage is a function that we passed down from the parent component (App)
+    document.title = currentPage; //set the document title to the currentPage prop
+  }, [currentPage]); //this is the second argument to useEffect. It's an array of dependencies.
+  //In this case, we're passing in the currentPage prop
+
+  //{currentPage === "about" ? "mx-2 navActive" : "mx-2"} is a ternary operator
+  //that will return the string "mx-2 navActive" if the current page is "about"
+  //so basically one click it runs a different className for color on click
+  // ternary operator is a shortcut for if/else statements
+  // ? is the condition and : is the else
+  // really handy JSX syntax here! :)
+  //{() => setCurrentPage("about")}
+  //this is a function that will set the current page to "about"
+  //when the user clicks on the about link
+  //so basically when the nav buttons are clicked, were using arrow functions to set the current page to the page they clicked on
 
   return (
     <nav>
