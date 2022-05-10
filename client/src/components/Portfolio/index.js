5e9c89e323545762e9386e41b27efc504b192c47
HacAtac <jhack00@icloud.com>
2022-05-09 19:15:25 -0500

    update

--- client/src/components/Portfolio/index.js
+++ client/src/components/Portfolio/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import awsome from "../../assets/images/awsome.png";
-import game from "../../assets/images/game.png";
-import mongoAPI from "../../assets/images/mongoAPI.png";
-import WMS from "../../assets/images/WMS.png";
+import React from 'react'
+import awsome from '../../assets/images/awsome.png'
+//import game from '../../assets/images/game.png'
+import ecom from '../../assets/images/ecom.png'
+import mongoAPI from '../../assets/images/mongoAPI.png'
+import WMS from '../../assets/images/WMS.png'
 // import ecommerce from "../../assets/images/ecommerce.png";
-import budget from "../../assets/images/budget.png";
-import screenshot from "../../assets/images/screenshot.PNG";
+import budget from '../../assets/images/budget.png'
+import screenshot from '../../assets/images/screenshot.PNG'
 
 export default function Portfolio() {
   //here were going to make a an array of objects that will be used to display the portfolio
@@ -13,69 +14,69 @@ export default function Portfolio() {
   //we're going to use the object's name as the key and the object's image as the value
   const projects = [
     {
-      name: "AWSOME",
+      name: 'MERN Ecommerce',
+      image: ecom,
+      techtitle: 'Tech Used:',
+      description:
+        'Robust MERN ecom app with admin panels, pagination, payments etc...',
+      technologies: [
+        'MERN, Redux, Restful API, Payment, MVC, Authentication, Admin Panels, Reviews, Pagination, Search Query Bar Etc.',
+      ],
+      github: 'https://github.com/HacAtac/newShop',
+      deployed: 'https://ecomhacatac.herokuapp.com/',
+      id: '2',
+    },
+    {
+      name: 'AWSOME',
       image: awsome,
-      techtitle: "Tech Used:",
-      description: "FullStack App made for a client",
+      techtitle: 'Tech Used:',
+      description: 'FullStack App made for a client',
       technologies: [
-        "Node, Express, mySQL, sequelize, bcrypt, sessions, handlebars, Heroku",
+        'Node, Express, mySQL, sequelize, bcrypt, sessions, handlebars, Heroku',
       ],
-      github: "https://github.com/osbym/awsome-organics-gp2",
-      deployed: "https://awsomeorganics.herokuapp.com/",
-      id: "1",
+      github: 'https://github.com/osbym/awsome-organics-gp2',
+      deployed: 'https://awsomeorganics.herokuapp.com/',
+      id: '1',
     },
     {
-      name: "GTX-Mobile-Detailing",
-      image: screenshot,
-      techtitle: "Tech Used:",
-      description:
-        "MERN stack app to help a client manage his services and bookings",
-      technologies: [
-        "MongoDB, Mongoose, Express, GraphQL, Apollo, React, Node, Heroku, MVC architecture",
-      ],
-      github: "https://github.com/HacAtac/gtx-mobile-detailing",
-      deployed: "https://gtxdetailing.herokuapp.com/",
-      id: "5",
-    },
-    {
-      name: "API for a Social Media App",
+      name: 'API for a Social Media App',
       image: mongoAPI,
-      techtitle: "Tech Used:",
+      techtitle: 'Tech Used:',
       description:
-        "Backend CRUD API that utilizes sub documents, .populate(), .virtuals()",
+        'Backend CRUD API that utilizes sub documents, .populate(), .virtuals()',
       technologies: [
-        "JavaScript, Node, Express, mongoDB, mongoose, Insomnia, JSON, Heroku, MVC architecture",
+        'JavaScript, Node, Express, mongoDB, mongoose, Insomnia, JSON, Heroku, MVC architecture',
       ],
-      github: "https://github.com/HacAtac/hacSocializeApi",
-      deployed: "https://watch.screencastify.com/v/zXc3jY3S0y3Hv9QVfF1Y",
-      id: "3",
+      github: 'https://github.com/HacAtac/hacSocializeApi',
+      deployed: 'https://watch.screencastify.com/v/zXc3jY3S0y3Hv9QVfF1Y',
+      id: '3',
     },
     {
-      name: "Game Suggestion App ",
-      image: game,
-      techtitle: "Tech Used:",
+      name: 'GTX-Mobile-Detailing',
+      image: screenshot,
+      techtitle: 'Tech Used:',
       description:
-        "Colaborative app that uses Open Source API's to find their next game + game giveaways!",
+        'MERN stack app to help a client manage his services and bookings',
       technologies: [
-        "JS, Bulma, ServerSideAPI, AJAX, jQuery, moment.js, template literals, JSON, git workflow, ZenHub",
+        'MongoDB, Mongoose, Express, GraphQL, Apollo, React, Node, Heroku, MVC architecture',
       ],
-      github: "https://github.com/HacAtac/videogame-recommender",
-      deployed: "https://osbym.github.io/videogame-recommender/",
-      id: "2",
+      github: 'https://github.com/HacAtac/gtx-mobile-detailing',
+      deployed: 'https://gtxdetailing.herokuapp.com/',
+      id: '5',
     },
 
     {
-      name: "Workforce Management System",
+      name: 'Workforce Management System',
       image: WMS,
-      techtitle: "Tech Used:",
+      techtitle: 'Tech Used:',
       description:
-        "Backend CMS that allows for companies to create, update, and delete their own information",
+        'Backend CMS that allows for companies to create, update, and delete their own information',
       technologies: [
-        "JavaScript,  Node, Express, mySql, inquirer, console.table, Insomnia, JSON, ScreenCastify, MVC architecture",
+        'JavaScript,  Node, Express, mySql, inquirer, console.table, Insomnia, JSON, ScreenCastify, MVC architecture',
       ],
-      github: "https://github.com/HacAtac/hacEmployeeTracker",
-      deployed: "https://watch.screencastify.com/v/qRwBBmEJYXPRyPfbVsKU",
-      id: "4",
+      github: 'https://github.com/HacAtac/hacEmployeeTracker',
+      deployed: 'https://watch.screencastify.com/v/qRwBBmEJYXPRyPfbVsKU',
+      id: '4',
     },
 
     // {
@@ -92,19 +93,19 @@ export default function Portfolio() {
     //   id: "5",
     // },
     {
-      name: "Budget Tracker PWA",
+      name: 'Budget Tracker PWA',
       image: budget,
-      techtitle: "Tech Used:",
+      techtitle: 'Tech Used:',
       description:
-        "PWA that allows users to track their budget even offline, and uploads it to a database once connected",
+        'PWA that allows users to track their budget even offline, and uploads it to a database once connected',
       technologies: [
-        "mongoDB, mongoose, express, indexedDB, service-workers, web-manifest, PWA, JS, CSS, Heroku ",
+        'mongoDB, mongoose, express, indexedDB, service-workers, web-manifest, PWA, JS, CSS, Heroku ',
       ],
-      github: "https://github.com/HacAtac/hacBudgetTracPWA",
-      deployed: "https://hacbudget.herokuapp.com/",
-      id: "6",
+      github: 'https://github.com/HacAtac/hacBudgetTracPWA',
+      deployed: 'https://hacbudget.herokuapp.com/',
+      id: '6',
     },
-  ];
+  ]
 
   //6 cards that utilize flex and .map to show projects 2x3
   //that display the project name, image, description, technologies, github, and deployed link
@@ -112,49 +113,49 @@ export default function Portfolio() {
   //in a different size depending on the screen size
   return (
     <div>
-      <h1 id="state-title" className="Portfolio Projects">
-        {" "}
+      <h1 id='state-title' className='Portfolio Projects'>
+        {' '}
         My Work
       </h1>
-      <div className="flex flex-wrap justify-center">
+      <div className='flex flex-wrap justify-center'>
         {projects.map((project) => (
           <div
-            id="animate-card"
-            className="w-full md:w-1/2 lg:w-1/3 p-4 skew-y-12 "
+            id='animate-card'
+            className='w-full md:w-1/2 lg:w-1/3 p-4 skew-y-12 '
             key={project.id}
           >
-            <div className="bg-grey border-2 border-gray-400 rounded-lg shadow-lg p-4 ">
-              <h2 id="card-title" className="text-2xl font-bold text-center">
+            <div className='bg-grey border-2 border-gray-400 rounded-lg shadow-lg p-4 '>
+              <h2 id='card-title' className='text-2xl font-bold text-center'>
                 {project.name}
               </h2>
               <img
-                id="card-image"
-                className="w-full"
+                id='card-image'
+                className='w-full'
                 src={project.image}
                 alt={project.name}
-                style={{ maxHeight: "200px" }}
+                style={{ maxHeight: '200px' }}
               />
-              <p className="text-center">{project.description}</p>
-              <h4 className="text-center" id="tech-title">
+              <p className='text-center'>{project.description}</p>
+              <h4 className='text-center' id='tech-title'>
                 {project.techtitle}
               </h4>
-              <p className="text-center">{project.technologies}</p>
-              <div className="flex justify-center">
+              <p className='text-center'>{project.technologies}</p>
+              <div className='flex justify-center'>
                 <a
-                  id="github-link"
+                  id='github-link'
                   href={project.github}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className=" font-bold py-2 px-4 rounded-full"
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className=' font-bold py-2 px-4 rounded-full'
                 >
                   Github
                 </a>
                 <a
-                  id="deployed-link"
+                  id='deployed-link'
                   href={project.deployed}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="font-bold py-2 px-4 rounded-full ml-4"
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='font-bold py-2 px-4 rounded-full ml-4'
                 >
                   Deployed
                 </a>
@@ -164,7 +165,7 @@ export default function Portfolio() {
         ))}
       </div>
     </div>
-  );
+  )
 }
 
 // return (
--- client/src/components/Resume/index.js
+++ client/src/components/Resume/index.js
@@ -1,27 +1,33 @@
 // This page will have a link to my downloadable pdf resume
 // Then it will have a list of my proficiencies on front end in one container and back end in another container
 
-import React from "react";
+import React from 'react'
 
 export default function Resume() {
   return (
-    <div className="container">
-      <h1 id="resume-title">Resume</h1>
-      <div className="flex flex-wrap justify-between">
-        <div className="bottom-spacing">
-          <a href={require("../../assets/files/resume.pdf").default} download>
-            {" "}
+    <div className='container'>
+      <h1 id='resume-title'>Resume</h1>
+      <div className='flex flex-wrap justify-between'>
+        <div className='bottom-spacing'>
+          <a href={require('../../assets/files/resume1.pdf').default} download>
+            {' '}
             {/*I needed to add .default here to get it to download. It was showing up as empty without it */}
-            <h4 className="animate-pulse" id="color-4-download">
+            <h4 className='animate-pulse' id='color-4-download'>
               Download my Resume
             </h4>
           </a>
         </div>
       </div>
-      <div className="flex flex-wrap justify-center">
-        <div className="col-md-6 mr-20 pt-10">
-          <h3 id="front-end-title">Front End</h3>
+      <div className='flex flex-wrap justify-center'>
+        <div className='col-md-6 mr-20 pt-10'>
+          <h3 id='front-end-title'>Front End</h3>
           <ul>
+            <li>React</li>
+            <li>Redux</li>
+            <li>React Router</li>
+            <li>React-Bootstrap</li>
+            <li>React-Router-Dom</li>
+            <li>Axios</li>
             <li>HTML5</li>
             <li>CSS3</li>
             <li>JavaScript</li>
@@ -34,12 +40,12 @@ export default function Resume() {
             <li>Bootstrap</li>
             <li>Media Queries</li>
             <li>Responsive Design</li>
-            <li>React</li>
+
             <li></li>
           </ul>
         </div>
-        <div className="col-md-6 mr-20 pt-10">
-          <h3 id="back-end-title">Back End</h3>
+        <div className='col-md-6 mr-20 pt-10'>
+          <h3 id='back-end-title'>Back End</h3>
           <ul>
             <li>Node.js</li>
             <li>Express</li>
@@ -50,7 +56,7 @@ export default function Resume() {
             <li>MongoDB Atlas</li>
             <li>Postman</li>
             <li>Heroku</li>
-            <li>GitHub</li>
+            <li>Git</li>
             <li>Jest</li>
             <li>OOP</li>
             <li>MVC</li>
@@ -64,5 +70,5 @@ export default function Resume() {
         </div>
       </div>
     </div>
-  );
+  )
 }
